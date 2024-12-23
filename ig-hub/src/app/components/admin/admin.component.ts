430b82268bf588c1af5dc7805bdd4be0dbe4cf9a
ibrahim-sayys <syedibrahimik3551@gmail.com>
2024-12-23 23:58:04 +0500

    Write admin logic

--- ig-hub/src/app/components/admin/admin.component.ts
+++ ig-hub/src/app/components/admin/admin.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CarService } from '../../services/car.service';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent {
+  newCar: any = {
+    name: '',
+    model: '',
+    brand: '',
+    status: '',
+    price: null,
+    image: '',
+  };
+  cars: any[] = [];
+  brands: string[] = ['Toyota', 'Honda', 'Ford', 'BMW', 'Mercedes']; // Predefined list of car brands.
 
+  constructor(private carService: CarService) {
+    this.carService.cars$.subscribe((data) => {
+      this.cars = data;
+    });
+  }
+
+  addCar() {
+    this.carService.addCar({ ...this.newCar });
+    this.newCar = {
+      name: '',
+      model: '',
+      brand: '',
+      status: '',
+      price: null,
+      image: '',
+    };
+  }
+
+  removeCar(index: number) {
+    this.carService.removeCar(index);
+  }
+
+  onImageSelect(event: any) {
+    const file = event.target.files[0];
+    this.newCar.image = file ? URL.createObjectURL(file) : '';
+  }
 }
