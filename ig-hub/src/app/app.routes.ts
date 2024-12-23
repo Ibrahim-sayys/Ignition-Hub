d9e8efe19da40fb2e9bc5439f375df5c17a55591
ibrahim-sayys <syedibrahimik3551@gmail.com>
2024-12-23 16:20:25 +0500

    Generate required components and enable routing

--- ig-hub/src/app/app.routes.ts
+++ ig-hub/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { CatagoriesComponent } from './components/catagories/catagories.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'categories', component: CatagoriesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+];
--- ig-hub/src/main.ts
+++ ig-hub/src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/components/home/home.component';
+import { AboutComponent } from './app/components/about/about.component';
+import { CatagoriesComponent } from './app/components/catagories/catagories.component';
+import { ContactComponent } from './app/components/contact/contact.component';
+import { LoginComponent } from './app/components/login/login.component';
+import { AdminComponent } from './app/components/admin/admin.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'categories', component: CatagoriesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
