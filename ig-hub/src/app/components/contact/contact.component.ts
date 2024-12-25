a0b19e4ac0cb583fa8fcb814b93815c381889b5e
ibrahim-sayys <syedibrahimik3551@gmail.com>
2024-12-25 23:17:47 +0500

    Build contact logic

--- ig-hub/src/app/components/contact/contact.component.ts
+++ ig-hub/src/app/components/contact/contact.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+  messageSent = false;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]+$')]],
+      age: ['', [Validators.required, Validators.min(1)]],
+      idNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      query: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      this.messageSent = true;
+      this.contactForm.reset();
+    }
+  }
 }
